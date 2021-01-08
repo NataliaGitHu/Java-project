1e90d0c581406adbc69b40884ee9fdf197145b40
Natalia <natalya@bigbrassband.com>
2021-01-08 18:00:54 +0600

    Second lesson. Exercise 2. Class Point

---
+++ sandbox/src/main/java/ru/java/Point.java
@@ -0,0 +1,26 @@
+package ru.java;
+
+class Point {
+    // поля класса
+    public double x;
+    public double y;
+
+    // конструктор
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double d = Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+        return d;
+    }
+        public static void main(String[] args) {
+            Point p1 = new Point(1.4, 1.7);
+            Point p2 = new Point(2.8, 2.5);
+            double d = Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+            System.out.println("Расстояние между двумя точками составляет " + d);
+        }
+    }
+
+
