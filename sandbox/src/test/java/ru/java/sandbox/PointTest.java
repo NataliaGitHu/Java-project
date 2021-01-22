54b6fe858052b67293655a6661cf7576a78d24ba
Natalia <natalya@bigbrassband.com>
2021-01-22 14:23:31 +0600

    PointTest

--- sandbox/src/test/java/ru/java/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/java/sandbox/PointTest.java
@@ -17,16 +17,16 @@ public class PointTest {
     // отрицательные числа
     @Test
     public void testDistance1() {
-        Point p3 = new Point(-2.0, -3.0);
-        Point p4 = new Point(-1.0, -7.0);
-        Assert.assertEquals(Point.distance(p3, p4), 4.123105625617661);
+        Point p1 = new Point(-2.0, -3.0);
+        Point p2 = new Point(-1.0, -7.0);
+        Assert.assertEquals(Point.distance(p1, p2), 4.123105625617661);
     }
 
     // двузначные числа
      @Test
     public void testDistance3() {
-        Point p5 = new Point(11.0, 25.0);
-        Point p6 = new Point(10.0, 15.0);
-        Assert.assertEquals(Point.distance(p5, p6), 10.04987562112089);
+        Point p1 = new Point(11.0, 25.0);
+        Point p2 = new Point(10.0, 15.0);
+        Assert.assertEquals(Point.distance(p1, p2), 10.04987562112089);
     }
 }
